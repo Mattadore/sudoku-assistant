dc6b4ec229512ae1618f3ef6b444c9a9c7de7899
Matt Buckley <me@matt.software>
2020-08-14 12:21:55 -0500

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // Add typescript stack into webpack
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-emotion`,
   ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,24 +13,17 @@ interface IndexPageProps {
   }
 }
 
-export default class extends React.Component<IndexPageProps, {}> {
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context)
-  }
-  public render() {
-    return (
-      <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new{' '}
-          <strong>{this.props.data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </div>
-    )
-  }
-}
+export default (props: IndexPageProps, context: any) => (
+  <div>
+    <h1>Hi people</h1>
+    <p>
+      Welcome to your new <strong>{props.data.site.siteMetadata.title}</strong>{' '}
+      site.
+    </p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
 
 export const pageQuery = graphql`
   query IndexQuery {
