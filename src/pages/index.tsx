655d91fdafd7489b27f6d3515039d382b90e3c3c
Matt Buckley <me@matt.software>
2020-08-14 12:23:47 -0500

    Import gql tag dirctly to calm a warning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Link from 'gatsby-link'
+import { graphql } from 'gatsby'
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
